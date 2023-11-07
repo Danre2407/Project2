61f87a833b4608034f01ee98ef14b090f703904c
Danre2407 <daniel.rettbergsoler@gmail.com>
2023-11-07 12:22:50 +0100

    push 2

---
+++ project-docker/flask/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('/swagger/index.html')
+
+@app.route('/hi')
+def test():
+   return "This is a test page"
+
+if __name__ == '__main__':
+   app.run()
